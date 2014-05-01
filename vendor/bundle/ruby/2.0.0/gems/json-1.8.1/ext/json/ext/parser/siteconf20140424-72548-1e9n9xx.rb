23372ed61bd9ac2fe00cedd424a2bbf681f99ff1
Aaron Stewart <aaronstewart@Aarons-MacBook-Pro.local>
2014-05-01 16:45:32 -0600

    Created a status blog using a scaffold and bootsrap. I also updated some of the layout.

---
+++ vendor/bundle/ruby/2.0.0/gems/json-1.8.1/ext/json/ext/parser/siteconf20140424-72548-1e9n9xx.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140424-72548-3wn6d0"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
